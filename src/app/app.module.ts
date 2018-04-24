875f7accb607835367e22317fecc897d0ed09dc3
Esteban Mayoral Poncelas <esteban.mayoral@pt-emayoral.panel.es>
2018-04-24 10:31:07 +0200

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListaTrabajoComponent } from './lista-trabajo/lista-trabajo.component';
+import { ListaTrabajosComponent } from './lista-trabajos/lista-trabajos.component';
+import { FormComponent } from './form/form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TrabajoInfoComponent } from './trabajo-info/trabajo-info.component';
+import { TrabajoComponent } from './trabajo/trabajo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaTrabajoComponent,
+    ListaTrabajosComponent,
+    FormComponent,
+    NavbarComponent,
+    TrabajoInfoComponent,
+    TrabajoComponent
   ],
   imports: [
     BrowserModule
